7cb6c1ac96c180335babc039a8ecc6861f5bad4b
Charlie Dobbie <cfmdobbie@gmail.com>
2016-04-19 00:00:00 +0100

    Commit of version 0.0.1

---
+++ prototypes/item.lua
@@ -0,0 +1,14 @@
+data:extend({
+
+ {
+    type = "item",
+    name = "bullet-bill",
+    icon = "__BulletBillTrain__/graphics/icon_bulletbill.png",
+    flags = { "goes-to-quickbar" },
+    subgroup = "transport",
+    place_result="bullet-bill",
+    stack_size= 5,
+  }
+
+})
+
---
+++ prototypes/recipe.lua
@@ -0,0 +1,13 @@
+data:extend({
+ {
+    type = "recipe",
+    name = "bullet-bill",
+    enabled = "true",
+    ingredients = 
+    {
+      {"diesel-locomotive",1},
+      {"piercing-bullet-magazine",1}
+    },
+    result = "bullet-bill"
+  }
+})
